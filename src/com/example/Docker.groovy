7ed0446d1939c3d7f5195a688a8d9725a4bcb151
Neil Everette <neil.everette@gmail.com>
2024-01-18 22:19:36 -0600

    bug edits

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -8,12 +8,18 @@ class Docker implements Serializable {
         this.script = script
     }
     def buildDockerImage(String imageName) {
-        script.echo 'building the docker image'
+        script.echo "building the docker image"
+        script.sh "docker build -t $imageName ."
+    }
+
+    def dockerLogin(){
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable:'USER')]){
-            script.sh "docker build -t $imageName ."
             script.sh "echo '${script.PASS}'| docker login -u '${script.USER}' --password-stdin"
-            script.sh "docker push $imageName"
         }
+    }
+
+    def dockerPush(String imageName){
+        script.sh "docker push $imageName"
     }
 }
 
